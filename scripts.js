e7225fbe63978c4f4e823c12b6bfc0ced26f1ffd
Celeste <zdceleste@gmail.com>
2018-06-12 18:19:19 -0300

    Agregado de función cambiabackground (en proceso) y cambio de fuente

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
 /*
+Función para cambiar el background durante el juego
+*/
+
+function cambiaBackground() {
+    var images = ['Assets/2.png', 'Assets/3.png', 'Assets/4.png', 'Assets/5.png', 'Assets/6.png', 'Assets/7.png', 'Assets/8.png']
+
+    setInterval(function () {
+
+        document.getElementById("cambiabackground").style.backgroundImage = "url('" + images[0] + "')";
+
+        var firstValue = images.shift();
+        images.push(firstValue);
+
+
+    }, 12000);
+
+}
+
+/*
 Función para activar/desactivar el audio
 */
 
